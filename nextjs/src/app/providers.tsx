d0eba98d8b99ddee904a0f4ce97ee3a790a9fa68
thatcher thornberry <thatcherthornberry@gmail.com>
2026-02-06 11:24:33 -0700

    add ga provider e

--- nextjs/src/app/providers.tsx
+++ nextjs/src/app/providers.tsx
@@ -1,18 +1,21 @@
 import { AuthProvider } from "@/contexts/auth-context";
+import { GoogleAnalyticsProvider } from "@/providers/google-analytics-provider";
 import ProgressBarProvider from "@/providers/progressBarProvider";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ProgressBarProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <AuthProvider>{children}</AuthProvider>
-      </ThemeProvider>
-    </ProgressBarProvider>
+    <GoogleAnalyticsProvider>
+      <ProgressBarProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
+      </ProgressBarProvider>
+    </GoogleAnalyticsProvider>
   );
 }
